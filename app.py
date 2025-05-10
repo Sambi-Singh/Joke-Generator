f05032ec69105b3bfe91963426182c1a9444df71
Sambi <sambisingh289@gmail.com>
2025-05-10 11:52:11 -0500

    Now random jokes outputted to screen
    Fixed issue with formatting by adding header request

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 # NO API KEY REQUIRED, thanks icanhazdadjoke :)
 import requests
-joke_data = requests.get('https://icanhazdadjoke.com/')
-print(joke_data.status_code)
+Headers = {'Accept': 'application/json'}
+joke_data = requests.get('https://icanhazdadjoke.com/', headers=Headers) #Remeber to pass in header to specify right format
+#print(joke_data.status_code)
+joke = joke_data.json()['joke']
+print(joke)

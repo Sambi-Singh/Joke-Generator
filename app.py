c815bc4e478d182a804b98e9bde551a4f002bec7
Sambi <sambisingh289@gmail.com>
2025-05-11 09:50:10 -0500

    Added a loop to print more jokes, but issue is that its printing same joke over and over again :(

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ import requests
 Headers = {'Accept': 'application/json'}
 joke_data = requests.get('https://icanhazdadjoke.com/', headers=Headers) #Remeber to pass in header to specify right format
 #print(joke_data.status_code)
-joke = joke_data.json()['joke']
-print(joke)
+#joke = joke_data.json()['joke']
+#print(joke)
+getNumJokes = int(input("How many jokes do you want to laugh at?: "))
+for joke in range(getNumJokes):
+    joke = joke_data.json()['joke']
+    print(f"{joke}\n")
+
+

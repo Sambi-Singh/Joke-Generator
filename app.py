0148c1621cb4e950d8631e97a7565367ad9ac576
Sambi <sambisingh289@gmail.com>
2025-05-13 21:00:39 -0500

    Prints diff jokes now

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # NO API KEY REQUIRED, thanks icanhazdadjoke :)
 import requests
 Headers = {'Accept': 'application/json'}
-joke_data = requests.get('https://icanhazdadjoke.com/', headers=Headers) #Remeber to pass in header to specify right format
 #print(joke_data.status_code)
 #joke = joke_data.json()['joke']
 #print(joke)
 getNumJokes = int(input("How many jokes do you want to laugh at?: "))
 for joke in range(getNumJokes):
+    joke_data = requests.get('https://icanhazdadjoke.com/', headers=Headers) #Remeber to pass in header to specify right format
     joke = joke_data.json()['joke']
     print(f"{joke}\n")
 
